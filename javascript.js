34e7f7b256062d6f306d1f7de8fa538d91691d57
theboban5 <108588821+theboban5@users.noreply.github.com>
2023-09-26 16:10:26 +0200

    Add js

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let message;
+message = 'Hello'; //store the string 'Hello' in the variable named message
+
+alert(message); //shows the variable content
+
+let user = 'John';
+age = 25;
+message = 'Hello'; 
+
+const myBirthday = '01.01.1900'; //constant variable (won't change)
+
